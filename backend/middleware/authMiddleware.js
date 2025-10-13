68923940e21bec99facc26137d96cbd13abf66ed
draohenrick <agenciablocktool@gmail.com>
2025-10-13 18:03:45 -0300

    Create authMiddleware.js

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'dplay_super_secret_key_12345';
+
+function authMiddleware(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'Acesso negado. Nenhum token fornecido.' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(403).json({ error: 'Token inválido ou expirado.' });
+    }
+}
+
+module.exports = authMiddleware;
