82e74e9bce1ba579577ff8331ee5b83b1e73e9d2
draohenrick <agenciablocktool@gmail.com>
2025-10-13 22:34:46 -0300

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'dplay_super_secret_key_12345';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 function authMiddleware(req, res, next) {
     const authHeader = req.headers.authorization;
@@ -12,8 +12,8 @@ function authMiddleware(req, res, next) {
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
-        req.user = decoded;
-        next();
+        req.user = decoded; // Adiciona os dados do usuário (id, email, nome) ao objeto 'req'
+        next(); // Continua para a próxima rota
     } catch (error) {
         res.status(403).json({ error: 'Token inválido ou expirado.' });
     }
